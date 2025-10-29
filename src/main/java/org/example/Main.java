79a4c5d788cfa3d034212c00df08ea75b142da3e
Samuel Meijer <samuel.j.meijer@gmail.com>
2025-10-29 14:04:41 +0100

    Adding assignment 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,8 @@ public class Main {
         calculator.runCalculation();
         **** REMOVE COMMENT TO RUN ASSIGNMENT 1 **** */
 
+
+        /* **** REMOVE COMMENT TO RUN ASSIGNMENT 2 ****
         // Uppgift 2 - Betygskalkylator och Array-analys
         int[] myNumbs = {36,77,68,92,73};
 
@@ -19,5 +21,43 @@ public class Main {
 
             System.out.println("Poäng " + score + " ger betyg " + grade);
         }
+         **** REMOVE COMMENT TO RUN ASSIGNMENT 2 **** */
+
+
+        // Uppgift 3 - Enkelt Prisvalideringssystem
+        int personAge = 23;
+        String personStatus = "Student";
+
+        boolean ageStatus = validateAge(personAge);
+        boolean discountStatus = validateDiscount(personStatus);
+        double finalPrice = getPrice(100, ageStatus, discountStatus);
+        System.out.println("Personen är " + personAge + " år gammal och har status " + personStatus);
+        System.out.println("Det slutgiltiga priset är: " + finalPrice);
+    }
+
+    public static boolean validateAge (int age) {
+        if ( 18 <= age && age <= 65) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean validateDiscount (String status) {
+        return status == "Student" || status == "Pensionär";
+    }
+
+    public static double getPrice (double grundpris, boolean isTarget, boolean hasDiscount) {
+        double price;
+
+        if (isTarget && hasDiscount) {
+            price = grundpris * 0.75;
+        } else if (!isTarget) {
+            price = grundpris * 1.1;
+        } else {
+            price = grundpris;
+        }
+
+        return price;
     }
 }
