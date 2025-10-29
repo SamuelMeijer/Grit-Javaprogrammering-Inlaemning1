55a8561c96707ac661de6a0aa722cc5d54885ca2
Samuel Meijer <samuel.j.meijer@gmail.com>
2025-10-29 10:12:41 +0100

    Updating assignment 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,31 +4,34 @@ import java.util.Scanner;
 
 public class Main {
     static void main() {
-    // Uppgift 1 - Kalkylator
+        // Uppgift 1 - Kalkylator
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Chose a number");
+
+        System.out.println("Choose a number");
         int firstNumber = scanner.nextInt();
-        System.out.println("Chose a second number");
+        System.out.println("Choose a second number");
         int secondNumber = scanner.nextInt();
+        scanner.nextLine(); // Consuming the leftover new line from 'scanner.nextInt()'.
         System.out.println("Please input one of the following methods: addition, subtraction, multiplication, division, average");
-        // TODO: Borde funka och vara med scanner.nextLine() men det renderar bara i skip av input, varför?
-        String typeOfCalc = scanner.next();
+        String typeOfCalc = scanner.nextLine();
 
         switch (typeOfCalc) {
             case "addition":
-                System.out.println("Summan av talen är: " + (firstNumber + secondNumber));
+                System.out.println("Result: " + firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
                 break;
             case "subtraction":
-                System.out.println("Summan av talen är: " + (firstNumber - secondNumber));
+                System.out.println("Result: " + firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
                 break;
             case "multiplication":
-                System.out.println("Summan av talen är: " + (firstNumber * secondNumber));
+                System.out.println("Result: " + firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
                 break;
             case "division":
-                System.out.println("Summan av talen är: " + (firstNumber / secondNumber));
+                float quotientOfNumbers = (float) firstNumber / secondNumber;
+                System.out.println("Result: " + firstNumber + " / " + secondNumber + " = " + quotientOfNumbers);
                 break;
             case "average":
-                System.out.println("Summan av talen är: " + ((firstNumber + secondNumber) /2));
+                float averageOfNumbers = (float) (firstNumber + secondNumber) / 2;
+                System.out.println("Result: " + averageOfNumbers);
                 break;
             default: {
                 System.out.println("No valid method was chosen, can't generate a sum");
